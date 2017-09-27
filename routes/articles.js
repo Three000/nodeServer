6ffe23165c5e6f01dfb93997ca34b70f3c3d302c
baibing.zhang <baibing.zhang@56qq.com>
2017-09-27 14:50:03 +0800

    add

--- routes/articles.js
+++ routes/articles.js
@@ -3,13 +3,16 @@ var router = express.Router();
 var fs = require('fs');
 var bodyParser = require("body-parser");
 var app = express();
+var path = require('path');
 router.get('/', function(req, res, next) {
     res.send('请使用post方式提交')
 })
 
 class CreateHtml {
-    constructor(html) {
-        this.html = html;
+    constructor(options) {
+        this.html = options.content;
+        this.title = options.title || '';
+        this.pageId = options.pageId;
     }
     handlerHtml() {
         var html = `<!DOCTYPE html>
@@ -18,7 +21,7 @@ class CreateHtml {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta content="telephone=no" name="format-detection">
-    <title>预付卡购买与服务协议</title>
+    <title>${this.title}</title>
     <script type="text/javascript" src="https://s.56qq.com/staticBase/dist/20170421/js/flexible.js"></script>
     <link rel="stylesheet" type="text/css" href="https://s.56qq.com/staticBase/dist/20170421/css/wlqq.css"/>
     <style>
@@ -51,8 +54,9 @@ class CreateHtml {
         return html;
     }
     getProfile(cb) {
+        var pageName = this.pageId || 'index'
         var data = this.handlerHtml();
-        fs.writeFileSync('assets/index.html', data);
+        fs.writeFileSync('assets/' + pageName + '.html', data);
         cb();
     }
 }
@@ -62,13 +66,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 router.post('/', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     const obj = {
-        status: 'ok',
+        status: 'OK',
         msg: ''
     }
     var query = req.body;
     console.log(query)
     if (query.content) {
-        var profile = new CreateHtml(query.content);
+        var profile = new CreateHtml(query);
         profile.getProfile(() => {
             obj.msg = '上传成功';
             res.send(obj);
@@ -77,7 +81,24 @@ router.post('/', (req, res, next) => {
         obj.msg = '不能为空'
         res.send(obj);
     }
-
 })
+
+router.get('/download/:fileName', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    // 实现文件下载 
+    var fileName = req.params.fileName;
+    var filePath = path.join(__dirname, '../assets/' + fileName);
+    var stats = fs.statSync(filePath);
+    if (stats.isFile()) {
+        res.set({
+            'Content-Type': 'application/octet-stream',
+            'Content-Disposition': 'attachment; filename=' + fileName,
+            'Content-Length': stats.size
+        });
+        fs.createReadStream(filePath).pipe(res);
+    } else {
+        res.end(404);
+    }
+});
 
 module.exports = router;
